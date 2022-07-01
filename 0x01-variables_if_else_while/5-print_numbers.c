4267e0dd66e5b0853136349b112e58db5b8dc59f
abdul123abc <munirudeen03abdullah@gmail.com>
2022-07-01 11:43:07 +0100

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints numbers from 0 to 9\n
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+	for (i = 0 ; i < 10 ; i++)
+	{
+	printf("%d", i);
+	}
+	return (0);
+}

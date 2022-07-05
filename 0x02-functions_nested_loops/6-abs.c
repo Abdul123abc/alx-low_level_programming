62af9403c53a5163cb76a80325a9a633c9b7d503
abdul123abc <munirudeen03abdullah@gmail.com>
2022-07-05 16:05:04 +0100

    absolutely

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,6 +4,7 @@
 /**
  * _abs - Entry point
  * Description: prints the absolute value of a number
+ * @n: n is an integer
  * Return: 0
  */
 int _abs(int n)
@@ -11,14 +12,14 @@ int _abs(int n)
 
 if (n > 0)
 {
-		return (n);
-	}
-	else if (n < 0)
-	{
-		return (-n);
-	}
-	else
-	{
-		return (n);
-	}
+return (n);
+}
+else if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
 }

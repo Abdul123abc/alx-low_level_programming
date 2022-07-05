aec32ffed730fb1464e6a1548f33e5e4dfcea0c1
abdul123abc <munirudeen03abdullah@gmail.com>
2022-07-05 15:05:55 +0100

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _isalpha - Entry point
+ * Description: checks is a character is lowercase or uppercase
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
  */
-int main(void)
+int _isalpha(int c)
 {
-    int n;
-
-    n = add(89, 9);
-    printf("%d\n", n);
-    return (0);
+int i = 'a';
+int I = 'A';
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i == c)
+return (1);
+}
+for (I = 'A'; I <= 'Z'; I++)
+{
+if (c == I)
+return (1);
+}
+return (0);
 }

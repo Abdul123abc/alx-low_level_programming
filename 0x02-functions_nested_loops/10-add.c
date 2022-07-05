101bfb8b9ff234ac2c9ece3da281efca780a36e3
abdul123abc <munirudeen03abdullah@gmail.com>
2022-07-05 21:09:55 +0100

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,11 +4,13 @@
 
 /**
  * add - check the code
- *
+ * @a: the integer value it receives
+ * @b: the integer value it receives
  * Return: Always 0.
  */
 int add(int a, int b)
 {
-int c = a + b;
-    return (c);
+	int c = a + b;
+
+	return (c);
 }
